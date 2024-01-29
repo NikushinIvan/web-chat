c7b8cfc2cb6caa143d6c2203ec28c4ecdd1ce385
Nikushin I.M <ivan.nikushin.99@mail.ru>
2024-01-29 16:34:02 +0300

    - В файле web.xml добавлен фильтр - Реализована логика класса LoginFilter - Реализована логика класса LoginCommand

--- src/main/java/org/example/command/action/LoginCommand.java
+++ src/main/java/org/example/command/action/LoginCommand.java
@@ -1,11 +1,14 @@
 package org.example.command.action;
 
 import org.example.command.Command;
+import org.example.data.DataBase;
+import org.example.data.User;
 import org.example.result.RedirectResult;
 import org.example.result.Result;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import static org.example.Resources.COMMAND_SHOW_CHAT_PAGE;
 
@@ -15,6 +18,14 @@ public class LoginCommand implements Command {
     public Result execute(HttpServletRequest request, HttpServletResponse response) {
 
         // Ваша реализация выполнения входа в Чат
+        String login = request.getParameter("loginInput");
+        String password = request.getParameter("passwordInput");
+
+        User user = DataBase.getUser(login, password);
+        if (user != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+        }
 
         return new RedirectResult(COMMAND_SHOW_CHAT_PAGE);
     }
--- src/main/java/org/example/filter/LoginFilter.java
+++ src/main/java/org/example/filter/LoginFilter.java
@@ -1,5 +1,8 @@
 package org.example.filter;
 
+import org.example.data.DataBase;
+import org.example.data.User;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -7,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static org.example.Resources.PAGE_LOGIN;
@@ -21,11 +25,15 @@ public class LoginFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
         // Ваша релизация фильтра входа пользователя на сайт
+        HttpServletRequest hsr = (HttpServletRequest) request;
+        HttpSession session = hsr.getSession();
 
-        if (true) { // Проверка, что пользователь авторизован (необходимо реализовать!!!)
-            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-            httpServletRequest.getRequestDispatcher(PAGE_LOGIN).forward(request, response);
-            return;
+        if (session.getAttribute("user") == null) {
+            if (!"login".equals(request.getParameter("command"))) {
+                HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+                httpServletRequest.getRequestDispatcher(PAGE_LOGIN).forward(request, response);
+                return;
+            }
         }
 
         filterChain.doFilter(request, response);
